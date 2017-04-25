1327d3cc1c246a9d6a7a845055970ee3aa9a6977
fedorath <kurtax.h1@googlemail.com>
2017-04-25 19:50:39 +0100

    hi

---
+++ Motors.py
@@ -0,0 +1,121 @@
+##########################################################################################################
+##########################################################################################################
+###################################   Kurtis Hall 14019369 Project   #####################################
+##########################################################################################################
+##########################################################################################################
+
+import RPi.GPIO as GPIO
+import time
+from Adafruit_PWM_Servo_Driver import PWM
+
+GPIO.setwarnings(False)
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(11, GPIO.IN) #Right IR set to 11
+GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #Front activates
+GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Left IR set to 7
+
+GPIO.setup(26,GPIO.OUT) #Left forward motor
+GPIO.setup(24,GPIO.OUT) #Left reverse motor
+GPIO.setup(19,GPIO.OUT) #Right forward motor
+GPIO.setup(21,GPIO.OUT) #Right reverse motor
+
+LF = GPIO.PWM(26, 50)#sets amount of power
+LF.start(0)
+
+LR = GPIO.PWM(24, 50)#sets amount of power
+LR.start(0)
+
+RF = GPIO.PWM(19, 20)#sets amount of power
+RF.start(0)
+
+RR = GPIO.PWM(21, 20)#sets amount of power
+RR.start(0)
+
+speed = 100#set speed
+
+flag=0
+while True:
+	j=GPIO.input(13)
+	if j==1: #Robot is activated when button is pressed
+		flag=1
+		print "Robot Activated",j
+
+	while flag==1:
+		i=GPIO.input(7)
+		k=GPIO.input(11)
+		if i==0:
+			print "Right Side Detected!",i
+			#Reverse
+    			LF.ChangeDutyCycle(0)
+    			LR.ChangeDutyCycle(speed)
+    			RF.ChangeDutyCycle(0)
+    			RR.ChangeDutyCycle(speed)
+    			LR.ChangeFrequency(speed + 5)
+    			RR.ChangeFrequency(speed + 5)
+			#Turns Left
+    			LF.ChangeDutyCycle(0)
+   			LR.ChangeDutyCycle(speed)
+   			RF.ChangeDutyCycle(speed)
+    			RR.ChangeDutyCycle(0)
+    			LR.ChangeFrequency(speed + 5)
+    			RF.ChangeFrequency(speed + 5)
+
+
+		if k==0: #Left IR
+			print "Left Side Detected",k
+			#Reverse
+    			LF.ChangeDutyCycle(0)
+    			LR.ChangeDutyCycle(speed)
+    			RF.ChangeDutyCycle(0)
+    			RR.ChangeDutyCycle(speed)
+    			LR.ChangeFrequency(speed + 5)
+    			RR.ChangeFrequency(speed + 5)
+			#Turns Right
+    			LF.ChangeDutyCycle(speed)
+    			LR.ChangeDutyCycle(0)
+    			RF.ChangeDutyCycle(0)
+    			RR.ChangeDutyCycle(speed)
+    			LF.ChangeFrequency(speed + 5)
+    			RR.ChangeFrequency(speed + 5)
+		elif i==0 and k==0:
+			print "Left and Right Detected!!"
+			#Reverse
+    			LF.ChangeDutyCycle(0)
+    			LR.ChangeDutyCycle(speed)
+    			RF.ChangeDutyCycle(0)
+    			RR.ChangeDutyCycle(speed)
+    			LR.ChangeFrequency(speed + 5)
+    			RR.ChangeFrequency(speed + 5)
+			#Turns Right
+    			LF.ChangeDutyCycle(speed)
+    			LR.ChangeDutyCycle(0)
+    			RF.ChangeDutyCycle(0)
+    			RR.ChangeDutyCycle(speed)
+    			LF.ChangeFrequency(speed + 5)
+    			RR.ChangeFrequency(speed + 5)
+			time(4)
+		elif i==1 and k==1:
+			print "Clear Route",i
+			#Forward
+    			LF.ChangeDutyCycle(speed)
+    			LR.ChangeDutyCycle(0)
+    			RF.ChangeDutyCycle(speed)
+    			RR.ChangeDutyCycle(0)
+    			LF.ChangeFrequency(speed + 5)
+    			RF.ChangeFrequency(speed + 5)
+		j=GPIO.input(13)
+		if j==1:
+			flag=0
+			print "Terminated!",j
+			#Stops
+    			LF.ChangeDutyCycle(0)
+    			LR.ChangeDutyCycle(0)
+    			RF.ChangeDutyCycle(0)
+    			RR.ChangeDutyCycle(0)
+				
+				
+##########################################################################################################
+#						The END!					         #
+##########################################################################################################
---
+++ SimpleCV.py
@@ -0,0 +1,93 @@
+##########################################################################################################
+##########################################################################################################
+###################################   Kurtis Hall 14019369 Project   #####################################
+##########################################################################################################
+##########################################################################################################
+
+#Multiple imports
+import os
+import time
+from SimpleCV import *
+import smtplib
+from datetime import datetime as dt
+from email.mime.image import MIMEImage
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+IMG = Camera()#Camera is intiated.
+Directory = "Photo" #Directory named photo
+if not os.path.exists("Photo"):#create a new directory
+	os.makedirs("Photo")#names it photo
+fmt = "%Y-%m-%d %H-%M-%S"#Date,Month,Year,Hour,Minute,Seconds
+
+##########################################################################################################
+#				Sending Attached PNG files to recipient.			         #
+##########################################################################################################
+
+def email(Gmail):
+
+	img_data = open(Gmail, 'rb').read()
+        msg = MIMEMultipart('mixed')
+        msg['Subject'] = 'Important Message!'#subject title
+        msg['From'] = 'kurtax.h1@googlemail.com'
+        msg['Reply-to'] = ', '.join('kurtax.h1@googlemail.com')
+        text = MIMEText("Intruder has been spotted!")#email body text
+        msg.attach(text)#attach body text
+        image = MIMEImage(img_data, name=os.path.basename(Gmail))
+        msg.attach(image)#attaches img
+
+        s = smtplib.SMTP('smtp.gmail.com', 587)#SMTP server connection
+        s.ehlo()
+        s.starttls()#Starts transport layer security
+        s.ehlo#Extended hello command
+        s.login('kurtax.h1@googlemail.com', 'kurtax%1')#Login Details
+        s.sendmail('kurtax.h1@googlemail.com','kurtax.h1@googlemail.com', msg.as_string())#Sending email
+        s.close()#Closes
+	
+##########################################################################################################
+#					SimpleCV Object detection.				         #
+##########################################################################################################
+
+	
+while True:#While loop which grabs images until it is told to stop.
+
+        Time = time.time()#time resets when loop
+	PIC = IMG.getImage()#original copy
+        PIC1 = IMG.getImage().toGray()#grayscale conversion
+	time.sleep(0.1)#waits before taking next photo(darker image)
+	PIC2 = IMG.getImage().toGray()#grayscale conversion
+	time.sleep(0.2)#sleeps
+        d = (PIC1 - PIC2).binarize(50).invert()#both images, - ,binarised,inverted.
+
+        matrix = d.getNumpy()#two values stored in matrix
+        avg = matrix.mean()#finds mean averags from the matrix
+	blobs = d.findBlobs()#scans for objects
+	
+	if Time >= time.time():#starts
+
+		for root, dirs, files in os.walk(Directory, topdown=False):#checks the folder for images
+			for file in files:#finds the image
+				Sortfile = sorted(files)[0]#sorts the images
+				mailer = os.path.join(root, Sortfile)
+				email(mailer)#sends image to email function
+				
+				
+	if avg >= 10:#average mean greater equal to 10
+
+		if blobs:
+
+			for blob in blobs:
+				try:
+					PIC.drawCircle((blob.x,blob.y),blob.radius(),SimpleCV.Color.GREEN,3)#draws green circle around object
+				except:
+					e = sys.exc_info()[0]
+					
+		name = dt.now().strftime(fmt) # filename using date, month, time
+
+		i = 1 #counter
+		PIC.save("Photo/Intruder%s-%s.png" % (name, i))#saves file with intruder and the date
+		print("Initiating Camera!")#prints to command line
+
+##########################################################################################################
+#						The END!					         #
+##########################################################################################################
